d31ecbf8b2baf69eed0e068d348a12ee457f9122
mark <mark>
2021-01-20 16:27:06 +0700

    lesson 5 - dependency injection

---
+++ lesson5/src/main/java/org/mark/Main.java
@@ -0,0 +1,16 @@
+package org.mark;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
+
+        context.close();
+    }
+}
