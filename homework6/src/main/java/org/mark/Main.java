043c1b381fa0dfbec61543159c49006d19e50699
mark <mark>
2021-01-20 17:45:27 +0700

    homewort for lesson 6 - dependency injection

---
+++ homework6/src/main/java/org/mark/Main.java
@@ -0,0 +1,18 @@
+package org.mark;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
+        System.out.println("Name: " + musicPlayer.getName());
+        System.out.println("Volume: " + musicPlayer.getVolume());
+
+        context.close();
+    }
+}
---
+++ homework6/src/main/java/org/mark/MusicPlayer.java
@@ -0,0 +1,37 @@
+package org.mark;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MusicPlayer {
+    private List<Music> musicList = new ArrayList<>();
+
+    private String name;
+    private int volume;
+
+    public void playMusic(){
+        musicList.forEach((Music music) ->{
+           System.out.println( music.getSong());
+        });
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+}
