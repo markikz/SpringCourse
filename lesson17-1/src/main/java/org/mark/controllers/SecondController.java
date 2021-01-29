d1e5e5e5112c0e20616968d0af9c23ad89cddbc2
mark <mark>
2021-01-29 17:46:58 +0700

    lesson 15

---
+++ lesson17-1/src/main/java/org/mark/controllers/SecondController.java
@@ -0,0 +1,17 @@
+package org.mark.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/second")
+public class SecondController {
+
+    @GetMapping("/exit")
+    public String exit(){
+        return "second/exit";
+    }
+
+
+}
