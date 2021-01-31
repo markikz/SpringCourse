da02b6b296f98d8ebba4209a945be92b1362ef1e
mark <mark>
2021-01-31 22:02:36 +0700

    lesson 22 - @ModelAttribute

--- lesson21/src/main/java/org/mark/controllers/PeopleController.java
+++ lesson21/src/main/java/org/mark/controllers/PeopleController.java
@@ -1,13 +1,11 @@
 package org.mark.controllers;
 
 import org.mark.dao.PersonDAO;
+import org.mark.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -38,6 +36,21 @@ public class PeopleController {
         //Получим одного человека по id из DAO и через модель передадим в представление
         return "people/show";
     }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person){
+
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person){
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+
+
+
 
 
 }
--- lesson21/src/main/java/org/mark/models/Person.java
+++ lesson21/src/main/java/org/mark/models/Person.java
@@ -11,6 +11,8 @@ public class Person {
         this.name = name;
     }
 
+    public Person(){}
+
     public int getId() {
         return id;
     }
